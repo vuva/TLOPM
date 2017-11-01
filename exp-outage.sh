eca256a93b779e6fe9aa4195905eca2881461bbd
vuva <vuanh.vu@ikt.uni-hannover.de>
2017-11-01 15:28:18 +0100

    fix single path exp $2 param

--- exp-outage.sh
+++ exp-outage.sh
@@ -2,6 +2,7 @@
 export n=$2
 export SL_ONTIME=5
 export SL_OFFTIME=3
+export SL_EXPTIME=$2
 export SL_FILE=$2
 EXP_TYPE=$1
 for ((i=1;i<=n;i++)); do
--- exp-sp.sh
+++ exp-sp.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-export n=$2
+export n=$3
 EXP_TYPE=$1
+export SL_EXPTIME=$2
+export SL_FILE=$2
 for ((i=1;i<=n;i++)); do
 echo "==== Running Test No. $i/$n ===="
 export SL_I=$i
