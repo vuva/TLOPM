2e65331534b742e41c8c8ee624ff0c7a5503c9d9
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2017-11-13 12:15:48 +0100

    fix node address

--- exp-outage.sh
+++ exp-outage.sh
@@ -9,33 +9,33 @@ for ((i=1;i<=n;i++)); do
 echo "==== Running Test No. $i/$n ===="
 export SL_I=$i
 
-ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=redundant'
 ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=redundant'
+ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=redundant'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-re-on"$SL_ONTIME"-off"$SL_OFFTIME
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
 sleep 20
 
 
-ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=roundrobin'
 ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=roundrobin'
+ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=roundrobin'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-rr-on"$SL_ONTIME"-off"$SL_OFFTIME
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
 sleep 20
 
-ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=default'
 ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=default'
+ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=default'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-lowrtt-on"$SL_ONTIME"-off"$SL_OFFTIME
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
 sleep 20
 
-ssh vuva@pc21.filab.uni-hannover.de 'sudo ip link set dev eth4 multipath off'
+ssh vuva@pc22.filab.uni-hannover.de 'sudo ip link set dev eth4 multipath off'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-sp-on"$SL_ONTIME"-off"$SL_OFFTIME
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
-ssh vuva@pc21.filab.uni-hannover.de 'sudo ip link set dev eth4 multipath on'
+ssh vuva@pc22.filab.uni-hannover.de 'sudo ip link set dev eth4 multipath on'
 sleep 20
 
 done
