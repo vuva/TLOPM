60c09ce901e366c073c556465dcf9301992f8f09
Vu Anh Vu <vuanh.vu@ikt.uni-hannover.de>
2017-11-14 17:23:27 +0100

    update DAG card

--- exp-outage.sh
+++ exp-outage.sh
@@ -13,7 +13,9 @@ ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=re
 ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=redundant'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-re-on"$SL_ONTIME"-off"$SL_OFFTIME
+ssh vuva@172.23.90.219 'sudo dagsnap -d0 -v -o '$SL_EX'.erf &'
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
+ssh vuva@172.23.90.219 'sudo killall -INT dagsnap'
 sleep 20
 
 
@@ -21,20 +23,26 @@ ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=ro
 ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=roundrobin'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-rr-on"$SL_ONTIME"-off"$SL_OFFTIME
+ssh vuva@172.23.90.219 'sudo dagsnap -d0 -v -o '$SL_EX'.erf &'
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
+ssh vuva@172.23.90.219 'sudo killall -INT dagsnap'
 sleep 20
 
 ssh vuva@pc22.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=default'
 ssh vuva@pc21.filab.uni-hannover.de 'sudo sysctl -w net.mptcp.mptcp_scheduler=default'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-lowrtt-on"$SL_ONTIME"-off"$SL_OFFTIME
+ssh vuva@172.23.90.219 'sudo dagsnap -d0 -v -o '$SL_EX'.erf &'
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
+ssh vuva@172.23.90.219 'sudo killall -INT dagsnap'
 sleep 20
 
 ssh vuva@pc22.filab.uni-hannover.de 'sudo ip link set dev eth2 multipath off'
 sleep 10
 export SL_EX=$EXP_TYPE"-interupted-sp-on"$SL_ONTIME"-off"$SL_OFFTIME
+ssh vuva@172.23.90.219 'sudo dagsnap -d0 -v -o '$SL_EX'.erf &'
 ~/sshlauncher/sshlauncher outage-$EXP_TYPE.config
+ssh vuva@172.23.90.219 'sudo killall -INT dagsnap'
 ssh vuva@pc22.filab.uni-hannover.de 'sudo ip link set dev eth2 multipath on'
 sleep 20
 
