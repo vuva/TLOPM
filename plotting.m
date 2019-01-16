7c923dc9f30866f180735ff159147ea41920de17
vuva <vuanh.vu@ikt.uni-hannover.de>
2019-01-16 11:42:05 +0100

    add cross traffic

--- plotting.m
+++ plotting.m
@@ -1,7 +1,7 @@
 %=========================== SET PARAMS ===================================
 k=1;
 n=1;
-prefix='D:\Data\conceal-ditg\';
+prefix='D:\Data\new-monkey-varpsize\';
 distribution_name = 'on5-off3';
 global exp_name;
 exp_name = 'conceal-ditg';
@@ -22,13 +22,18 @@ set(0,'DefaultLineLineWidth',1.5);
 lrtt_latency=[];
 rr_latency=[];
 re_latency=[];
-sp_latency=[];
-rbs_latency=[];
+opp_latency=[];
+tag8_latency=[];
+lazytail_0_latency=[];
+newmonkey1_latency=[];
+newmonkey50_latency=[];
+newmonkey100_latency=[];
+newmonkey20_latency=[];
+
 
 labels=[];
 for i=k:n
     lrtt_dat = csvread(strcat(prefix,exp_name,'-lowrtt-',num2str(i), '.dat' ));
-%     lrtt_dat=filterdata(lrtt_dat);
     lrtt_latency = vertcat(lrtt_latency,lrtt_dat(50:end-50,10));
     labels=[labels, "LowRTT"];
     
@@ -36,41 +41,101 @@ for i=k:n
     re_dat = csvread(strcat(prefix,exp_name,'-re-', num2str(i), '.dat' ));
     re_latency = vertcat(re_latency,re_dat(50:end-50,10));
     labels=[labels, "Redundant"];
+%     
+%     rr_dat = csvread(strcat(prefix,exp_name,'-rr-', num2str(i), '.dat' ));
+%     rr_latency = vertcat(rr_latency,rr_dat(50:end-50,10));
+%     labels=[labels, "RR"];
     
-    rr_dat = csvread(strcat(prefix,exp_name,'-rr-', num2str(i), '.dat' ));
-    rr_latency = vertcat(rr_latency,rr_dat(50:end-50,10));
-    labels=[labels, "RR"];
+    opp_dat = csvread(strcat(prefix,exp_name,'-opp-', num2str(i), '.dat' ));
+    opp_latency = vertcat(opp_latency,opp_dat(50:end-50,10));
+    labels=[labels, "opp"];
     
+%     tag8_dat = csvread(strcat(prefix,exp_name,'-tag-8-', num2str(i), '.dat' ));
+%     tag8_latency = vertcat(tag8_latency,tag8_dat(50:end-50,10));
+%     labels=[labels, "tag8"];
+    
+    lazytail_0_dat = csvread(strcat(prefix,exp_name,'-lazytail-0-', num2str(i), '.dat' ));
+    lazytail_0_latency = vertcat(lazytail_0_latency,lazytail_0_dat(50:end-50,10));
+    labels=[labels, "lazy-0"];
+    
+%     newmonkey1_dat = csvread(strcat(prefix,exp_name,'-newmonkey-1-', num2str(i), '.dat' ));
+%     newmonkey1_latency = vertcat(newmonkey1_latency,newmonkey1_dat(50:end-50,10));
+%     labels=[labels, "new-monkey-1"];
+    
+    newmonkey50_dat = csvread(strcat(prefix,exp_name,'-newmonkey-20-', num2str(i), '.dat' ));
+    newmonkey50_latency = vertcat(newmonkey50_latency,newmonkey50_dat(50:end-50,10));
+    labels=[labels, "new-monkey-20"];    
+    
+    newmonkey100_dat = csvread(strcat(prefix,exp_name,'-newmonkey-40-', num2str(i), '.dat' ));
+    newmonkey100_latency = vertcat(newmonkey100_latency,newmonkey100_dat(50:end-50,10));
+    labels=[labels, "new-monkey-40"];    
+    
+    newmonkey20_dat = csvread(strcat(prefix,exp_name,'-newmonkey-70-', num2str(i), '.dat' ));
+    newmonkey20_latency = vertcat(newmonkey20_latency,newmonkey20_dat(50:end-50,10));
+    labels=[labels, "new-monkey-70"];    
+       
 end
 
 %============================= PLOTS ======================================
+% [lrtt_latency,re_latency,opp_latency ,tag0_latency,tag8_latency] = filterdata(lrtt_latency,re_latency,opp_latency ,tag0_latency,tag8_latency);
+% plotccdf(labels,monkey0_latency,monkey2_latency,monkey4_latency,monkey8_latency,monkey16_latency);
+% [lrtt_latency, re_latency, opp_latency , lazytail_0_latency,newmonkey50_latency,newmonkey100_latency,newmonkey20_latency] = filterdata(lrtt_latency, re_latency, opp_latency , lazytail_0_latency,newmonkey50_latency,newmonkey100_latency,newmonkey20_latency);
+% plotccdf(labels, lrtt_latency, re_latency, opp_latency , lazytail_0_latency,newmonkey50_latency);
+% [monkey0_1_latency,monkey2_1_latency,monkey2_2_latency,monkey4_1_latency,monkey4_4_latency,monkey8_1_latency,monkey8_4_latency,monkey8_8_latency,monkey16_1_latency,monkey16_4_latency,monkey16_8_latency]=filterdata(monkey0_1_latency,monkey2_1_latency,monkey2_2_latency,monkey4_1_latency,monkey4_4_latency,monkey8_1_latency,monkey8_4_latency,monkey8_8_latency,monkey16_1_latency,monkey16_4_latency,monkey16_8_latency);
+% plotccdf(labels,lrtt_latency,re_latency,monkey0_1_latency,monkey2_1_latency,monkey2_2_latency,monkey4_1_latency,monkey4_4_latency,monkey8_1_latency,monkey8_4_latency,monkey8_8_latency,monkey16_1_latency,monkey16_4_latency,monkey16_8_latency);
+% plot_ref();
+% plot_avg_latency(labels, lrtt_latency, re_latency, opp_latency , lazytail_0_latency,newmonkey50_latency);
 
-plotccdf(labels,lrtt_latency,re_latency,rr_latency);
-plot_throughput(labels,lrtt_dat,re_dat,rr_dat);
+plot_quantile(0.05,labels, lrtt_latency, re_latency, opp_latency , lazytail_0_latency,newmonkey50_latency,newmonkey100_latency,newmonkey20_latency);
 
-% plot_subflows("LowRTT",lrtt_dat);
+% plotpdf(labels,lrtt_latency,re_latency,opp_latency ,tag0_latency,tag8_latency);
+% plot_throughput(labels,lrtt_dat, re_dat, opp_dat , tag8_dat, lazytail_8_dat, newmonkey1_dat);
+% boxplot_latency(labels,lrtt_latency,re_latency,opp_latency ,tag0_latency, monkey8_latency,monkey7_latency);
 % plot_subflows(rr_dat);
-plot_subflows("LowRTT",lrtt_dat);
-plot_subflows("Redundant",re_dat);
-plot_subflows("RR",rr_dat);
+% plot_subflows("LowRTT",lrtt_dat);
+% plot_subflows("Redundant",re_dat);
+% plot_subflows("tag0",tag0_dat);
+% plot_subflows("TAG8",tag8_dat);
+% plot_subflows("newmk1",newmonkey1_dat);
 
 %================================= END ====================================
 
 
-function [filteredData]= filterdata(data)
-        filteredData = data(data(:, 7) > 1.548823970274599e+09, :);
-        filteredData = filteredData(filteredData(:, 7) <1.549522290274599e+09, :);
+function []= plot_ref()
+    x1=50/1000;
+    line([x1 x1], get(gca, 'ylim'),'LineWidth',2, 'LineStyle','--','Color','Red','HandleVisibility','off');
+    
+    
+%     x2=40/1000;
+%     line([x2 x2], get(gca, 'ylim'),'LineWidth',2, 'LineStyle','--','Color','Red','HandleVisibility','off');
+%     
+%     x3=70/1000;
+%     line([x3 x3], get(gca, 'ylim'),'LineWidth',2, 'LineStyle','--','Color','Red','HandleVisibility','off');
+%     y1=.1/100;
+%     line(get(gca, 'xlim'),[y1 y1],'LineWidth',2, 'LineStyle','-.','Color','Red','HandleVisibility','off');   
+end
+
+function [varargout]= filterdata(varargin)
+    for i=1:nargin
+        varargout{i} = varargin{i}(varargin{i}(:, 1) > 0, :);
+        varargout{i} = varargout{i}(varargout{i}(:, 1) < 0.15, :);
+    end
+     
 end
 
 function[]=plotccdf(labels,varargin)
     global exp_name;
     
     figure
+    
+    
     for i=1:nargin-1
         [xccdf,yccdf]=getccdf(varargin{i});
         plot(xccdf,yccdf);
         hold on;
     end
+    xlabel('Latency (seconds)') ;
+    ylabel('Probability P(X>x)');
     title(strcat('CCDF-',exp_name));
     legend(labels);
     set(gca, 'YScale', 'log');
@@ -113,7 +178,7 @@ end
 
 function[thoughput]=get_throughput(sched_dat)
     global TIME_RESOLUTION;
-    TIME_COLUMM = 6;
+    TIME_COLUMM = 7;
     start_point = sched_dat(1,TIME_COLUMM);
     end_point=sched_dat(end,TIME_COLUMM) ;
     time_window= end_point - start_point;
@@ -147,6 +212,20 @@ function[] = plot_throughput(labels,varargin)
     set(gca,'XTickLabel',labels);
 end
 
+function[] = boxplot_latency(labels,varargin)
+    latencies=[];
+    group = [];
+    for i=1:nargin-1
+      
+        latencies=[latencies;varargin{i}];
+        group=[group;i*ones(size(varargin{i}))];
+    end
+    
+    figure
+    boxplot(latencies,group);
+    set(gca,'XTickLabel',labels);
+end
+
 function[] = plot_subflows(plot_title,flow_data)
     
     [sf_group,sf_senders] = findgroups(flow_data(:,2));
@@ -156,18 +235,63 @@ function[] = plot_subflows(plot_title,flow_data)
         plot(sf_throughput{i,1}(:,1), sf_throughput{i,1}(:,2));
         hold on;
     end
+    legend(dec2ip(sf_senders));
+    
 %     flow_throughput=get_throughput(flow_data);
-%     figure
 %     plot(flow_throughput(:,1), flow_throughput(:,2));
 %     hold on;
 %     legend("All");
-    title(plot_title);
-    legend(dec2ip(sf_senders));
 
+
+     title(plot_title);
     
 end
+
+
 
 function[ip] = dec2ip(decip)
     ip= strcat( num2str(bitand(bitshift(decip,-24), 255)) ,'.', num2str(bitand(bitshift(decip,-16), 255)) ,'.', num2str(bitand(bitshift(decip,-8), 255))  ,'.', num2str(bitand(bitshift(decip,0), 255)));  
 end
 
+function[] = plotpdf(labels, varargin)
+    global exp_name;
+    figure
+    for i=1:nargin-1
+        [fi,xi] = ksdensity(varargin{i});
+        plot(xi,fi);
+        hold on;
+    end
+    title(strcat('PDF-',exp_name));
+    legend(labels);
+%     set(gca, 'YScale', 'log');
+end
+
+function[] = plot_avg_latency(labels, varargin)
+    global exp_name;
+    bar_data=[];
+    
+    for i=1:nargin-1
+        bar_data = [bar_data, mean(varargin{i})];
+        hold on;
+    end
+    figure
+    bar(categorical(labels),bar_data);
+    title(strcat('avg-delay-',exp_name));
+    
+
+end
+
+function[] = plot_quantile(bound, labels, varargin)
+    global exp_name;
+    bar_data=[];
+    
+    for i=1:nargin-2
+        temp = varargin{i}(varargin{i}(:, 1) < bound, :);
+        bar_data = [bar_data, size(temp(:,1))/size(varargin{i}(:,1))];
+        hold on;
+    end
+    figure
+    bar(categorical(labels),bar_data);
+    title(strcat('P(X<50)-',exp_name));
+    
+end
